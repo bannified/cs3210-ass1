b9a81fe5ef507ea0af62ee40c870e40962b3fafb
Kuan Wei Heng <kuanwh1996@gmail.com>
2019-09-25 20:29:55 +0800

    Retab main.cpp

--- cs3210-ass1/cs3210-ass1/main.cpp
+++ cs3210-ass1/cs3210-ass1/main.cpp
@@ -1,8 +1,8 @@
 /**
-	CS3210 Assignment 1 Part 1 (OpenMP)
-	Authors:	Kuan Wei Heng (A.......)
-				Chong Jun Hong, Dominic (A0121237U)
-	std=C++11
+    CS3210 Assignment 1 Part 1 (OpenMP)
+    Authors: Kuan Wei Heng (A0121712X)
+             Chong Jun Hong, Dominic (A0121237U)
+    std=C++11
 */
 
 #include <vector>
@@ -14,31 +14,31 @@
 
 struct Vector2
 {
-	Vector2() { }
+        Vector2() { }
 
-	Vector2(double x, double y)
-		: x(x), y(y)
-	{
-	};
+        Vector2(double x, double y)
+                : x(x), y(y)
+        {
+        };
 
-	double x;
-	double y;
+        double x;
+        double y;
 };
 
 struct Particle
 {
-	Particle(Vector2 pos, Vector2 vel, uint32_t index)
-		: position(pos), velocity(vel), index(index)
-	{
-	};
+        Particle(Vector2 pos, Vector2 vel, uint32_t index)
+                : position(pos), velocity(vel), index(index)
+        {
+        };
 
-	Vector2 position;
-	Vector2 velocity;
+        Vector2 position;
+        Vector2 velocity;
 
-	uint32_t index;
+        uint32_t index;
 
-	uint32_t numWallCollisions;
-	uint32_t numParticleCollisions;
+        uint32_t numWallCollisions;
+        uint32_t numParticleCollisions;
 };
 
 Vector2 gStageSize;
@@ -55,47 +55,47 @@ inline void PrintParticle(const Particle particle);
 
 inline void PrintParticle(const Particle particle)
 {
-	printf("%i %i %10.8f %10.8f %10.8f\n %10.8f\n\n",
-		   particle.index,
-		   gStepNumber,
-		   particle.position.x,
-		   particle.position.y,
-		   particle.velocity.x,
-		   particle.velocity.y
-	);
+        printf("%i %i %10.8f %10.8f %10.8f\n %10.8f\n\n",
+                   particle.index,
+                   gStepNumber,
+                   particle.position.x,
+                   particle.position.y,
+                   particle.velocity.x,
+                   particle.velocity.y
+        );
 }
 
 int main(int argc, char *argv[])
 {
-	// Num of particles, Size of square, Radius of particle, and number of steps
-	int N, L;
-	scanf_s("%i", &N);
-	scanf_s("%i", &L);
-	gStageSize = Vector2(L, L);
-	scanf_s("%i", &gRadius);
-	scanf_s("%i", &gNumSteps);
+        // Num of particles, Size of square, Radius of particle, and number of steps
+        int N, L;
+        scanf_s("%i", &N);
+        scanf_s("%i", &L);
+        gStageSize = Vector2(L, L);
+        scanf_s("%i", &gRadius);
+        scanf_s("%i", &gNumSteps);
 
-	std::string inputBuffer;
-	scanf_s("%s", inputBuffer);
-	if (inputBuffer.compare("print")) {
-		// print for every timestep
-		gPrintAll = true;
-	}
+        std::string inputBuffer;
+        scanf_s("%s", inputBuffer);
+        if (inputBuffer.compare("print")) {
+                // print for every timestep
+                gPrintAll = true;
+        }
 
-	int runningIndex = 0;
-	particles.reserve(N);
+        int runningIndex = 0;
+        particles.reserve(N);
 
-	// Generate particles based on data
-	while (getline(std::cin, inputBuffer)) {
-		// TODO: Create particle
-	}
+        // Generate particles based on data
+        while (getline(std::cin, inputBuffer)) {
+                // TODO: Create particle
+        }
 
-	// Generate random particles
-	srand(time(NULL));
-	for (int i = runningIndex; i < N; i++) {
-		// TODO: Create random particles
+        // Generate random particles
+        srand(time(NULL));
+        for (int i = runningIndex; i < N; i++) {
+                // TODO: Create random particles
 
-	}
+        }
 
-	return 0;
+        return 0;
 }
