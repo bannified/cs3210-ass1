d9f9ad153ef558bf224aa846e630dca7b0084516
Kuan Wei Heng <kuanwh1996@gmail.com>
2019-09-28 15:27:43 +0800

    Add benchmarking script

---
+++ cs3210-ass1/cs3210-ass1/bench.py
@@ -0,0 +1,27 @@
+import subprocess
+
+BINARY = './a.out'
+IN_FILE_FORMAT = 'input-%d'
+
+THREAD_COUNTS = [1]
+PARTICLE_COUNTS = [10, 100, 1000]
+
+def run(command, particles, samples):
+    perf = '(perf stat -e cache-references -e cache-misses -e cycles -e instructions -- %s < %s > /dev/null) 2>&1' \
+        % (command, IN_FILE_FORMAT % particles)
+    output = subprocess.check_output(perf, shell=True)
+    _,_,_,a,b,c,d,_,e,_,_ = output.decode('utf-8').split('\n')
+    results = list(zip(*[[eval(s.strip().split()[0].replace(',','')) for s in (a,b,c,d,e)] for _ in range(samples)]))
+    cache_references = max(results[0])
+    cache_misses     = min(results[1])
+    cycles           = min(results[2])
+    instructions     = max(results[3])
+    time             = min(results[4]) 
+    return cache_references, cache_misses, cycles, instructions, time
+
+for threads in THREAD_COUNTS:
+    for particles in PARTICLE_COUNTS:
+        command = '%s %d' % (BINARY, threads)
+        cache_references, cache_misses, cycles, instructions, time = run(command, particles, 5)
+        print('Threads:', threads, 'Particles:', particles)
+        print(cache_references, cache_misses, cycles, instructions, time)
---
+++ cs3210-ass1/cs3210-ass1/gen.py
@@ -0,0 +1,22 @@
+from random import *
+
+PARTICLE_COUNTS = [10, 100, 1000]
+L = 20000
+r = 1
+steps = 100
+mode = "perf"
+
+for n in PARTICLE_COUNTS:
+    with open('input-%d' % n, 'w') as f:
+        print(n, file=f)
+        print(L, file=f)
+        print(r, file=f)
+        print(steps, file=f)
+        print(mode, file=f)
+        for i in range(n):
+            print(i,
+                  uniform(r, L-r),
+                  uniform(r, L-r),
+                  (-randint(0,1)|1) * uniform(L/4, L/8/r),
+                  (-randint(0,1)|1) * uniform(L/4, L/8/r),
+                  file=f)
